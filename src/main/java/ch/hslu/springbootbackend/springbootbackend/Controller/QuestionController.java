9fc5625502ee7d18cbb47589fafe8bd259d808d9
Fred <frederico.fischer@stud.hslu.ch>
2020-10-12 19:30:28 +0200

    Post Example with questionrepository

--- src/main/java/ch/hslu/springbootbackend/springbootbackend/Controller/QuestionController.java
+++ src/main/java/ch/hslu/springbootbackend/springbootbackend/Controller/QuestionController.java
@@ -1,7 +1,9 @@
 package ch.hslu.springbootbackend.springbootbackend.Controller;
 
+import ch.hslu.springbootbackend.springbootbackend.Entity.Answer;
 import ch.hslu.springbootbackend.springbootbackend.Exception.ResourceNotFoundException;
 import ch.hslu.springbootbackend.springbootbackend.Entity.Question;
+import ch.hslu.springbootbackend.springbootbackend.Repository.AnswerRepository;
 import ch.hslu.springbootbackend.springbootbackend.Repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,9 @@ public class QuestionController {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
+
     @GetMapping("/questions")
     public List<Question> getAllQuestions() {
         return questionRepository.findAll();
@@ -25,7 +30,18 @@ public class QuestionController {
 
     @PostMapping("/questions")
     public Question newQuestion(@RequestBody Question newQuestion) {
-        System.out.println(questionRepository.save(newQuestion));
+        List<Question> questions = questionRepository.findByQuestionphrase(newQuestion.getQuestionphrase());
+        if (!questions.isEmpty()) {
+            System.out.println("Question already exists!!!");
+            return questions.get(0);
+        }
+        for (Answer answer : newQuestion.getPossibleAnswers()) {
+            List<Answer> answers = answerRepository.findByAnswerPhrase(answer.getAnswerPhrase());
+            if (!answers.isEmpty()) {
+                System.out.println("Answer already exists!!!!");
+                // TODO: Replace the answer (in newQuestion) with the Answer which already exists!!!
+            }
+        }
         return questionRepository.save(newQuestion);
     }
     @GetMapping("/questions/{id}")
--- src/main/java/ch/hslu/springbootbackend/springbootbackend/Repository/QuestionRepository.java
+++ src/main/java/ch/hslu/springbootbackend/springbootbackend/Repository/QuestionRepository.java
@@ -4,6 +4,9 @@ import ch.hslu.springbootbackend.springbootbackend.Entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    List<Question> findByQuestionphrase(String questionphrase);
 }
