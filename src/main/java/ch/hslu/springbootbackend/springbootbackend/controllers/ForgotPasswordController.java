1aea2db19266bf9cffcf405d6bbf50ee0d72b5e9
Oliver Werlen <oliverwerlen@bluewin.ch>
2020-12-09 16:52:08 +0100

    fixed password reset

--- src/main/java/ch/hslu/springbootbackend/springbootbackend/controllers/ForgotPasswordController.java
+++ src/main/java/ch/hslu/springbootbackend/springbootbackend/controllers/ForgotPasswordController.java
@@ -2,14 +2,16 @@ package ch.hslu.springbootbackend.springbootbackend.controllers;
 
 import ch.hslu.springbootbackend.springbootbackend.Entity.User;
 import ch.hslu.springbootbackend.springbootbackend.Service.EntityService.UserService;
-import ch.hslu.springbootbackend.springbootbackend.Utility.Utility;
 import net.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
